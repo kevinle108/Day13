4e18a9ff321ddbd6e612932a9fe734c60e4e1e91
Kevin Le <kevinle108@gmail.com>
2021-03-15 10:22:47 -0400

    Finished ShrinkableWords prob16

--- Program.cs
+++ Program.cs
@@ -16,8 +16,46 @@ namespace Day13
                 "AC", "MTAFOC", "MD", "MTXABCD", "OA", "MITXAFEC", "OFMIT", "TED", "MTAEC",
                 "MITXFAEE", "TAF", "TAC", "AEOC", "TA", "MITXAFC", "ITXAFEC", "MITXAFE", "MITAFEC"
             };
-
+            ShrinkableWords("MITXAFEC", vocab);
+            
+        }
+        static bool ShrinkableWords(string word, List<string> vocab)
+        {
+            Stack<string> result = new Stack<string>();
+            if (ShrinkableWords(word, vocab, 0, result)) {
+                PrintResult(result);
+                return true;
+            } else return false;
         }
 
+        static bool ShrinkableWords(string word, List<string> vocab, int index, Stack<string> result)
+        {
+            if (word == "") return true;
+            if (!vocab.Contains(word))
+            {
+                return false;
+            }            
+            for (int i = 0; i < word.Length; i++)
+            {
+                string nextWord = word.Remove(i, 1);
+                result.Push(word + $" -> will remove {word[i]} next");
+                if (ShrinkableWords(nextWord, vocab, 0, result)) 
+                {
+                    return true;
+                } 
+                else result.Pop();
+            }
+            return false;
+        }
+
+        static void PrintResult(Stack<string> result)
+        {
+            foreach (string word in result)
+            {
+                Console.WriteLine(word);
+            }
+        }
     }
+
+
 }
