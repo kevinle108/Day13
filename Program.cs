49ee8cad6e69c65c7342a7c080cc146226edf140
Kevin Le <kevinle108@gmail.com>
2021-03-15 09:11:51 -0400

    added vocab for ShrinkableWords

--- Program.cs
+++ Program.cs
@@ -7,42 +7,17 @@ namespace Day13
     {
         static void Main(string[] args)
         {
-            List<int> myList = new List<int> { 3, 12, 25, 1, 8, 15, 2, 17 };
-            int sum = 30;
-            SubsetSum(myList, sum);
-        }
-
-        public static void SubsetSum(List<int> li, int sum)
-        {
-            var result = new Stack<int>();
-            if ( SubsetSum(li, 0, sum, result))
+            List<string> vocab = new List<string>()
             {
-                //result.ForEach(num => Console.WriteLine(num));
-                foreach (int num in result)
-                {
-                    Console.WriteLine(num);
-                }
-            }
+                "MTAO", "OF", "TEC", "MXD", "T", "O", "AF", "AE", "A",
+                "TAZ", "MTBXAE", "ABCDE", "TAEC", "MTOZE", "MITXFAEC", "MTAFC", "MTSFOC", "MTZABC",
+                "MTAFOC", "MITXFFEC", "XAEC", "MTAFEC", "F", "MTBXAEC", "ZAEC", "MFECFEC", "TAX",
+                "MTXAFEC", "TAFEC", "MXAFECI", "TAE", "ITXAFC", "MTAFOCC", "MITXAFEOC", "D", "MXITABFEC",
+                "AC", "MTAFOC", "MD", "MTXABCD", "OA", "MITXAFEC", "OFMIT", "TED", "MTAEC",
+                "MITXFAEE", "TAF", "TAC", "AEOC", "TA", "MITXAFC", "ITXAFEC", "MITXAFE", "MITAFEC"
+            };
 
         }
 
-        public static bool SubsetSum(List<int> li, int index, int sum, Stack<int> result)
-        {
-            if (sum == 0)
-            {
-                return true;
-            }
-            for (int i = index; i < li.Count; i++)
-            {
-                if (li[i] <= sum)
-                {
-                    result.Push(li[i]);
-                    if (SubsetSum(li, i + 1, sum - li[i], result)) return true;
-                    result.Pop();
-                }
-                
-            }
-            return false;
-        }
     }
 }
